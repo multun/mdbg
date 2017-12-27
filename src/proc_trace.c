fee684e23a421c2a17b549860ec68d17de3db082
Victor "multun" Collod <victor.collod@epita.fr>
2017-12-27 22:13:55 +0100

    proc_trace: add peek/poke primitives

--- src/proc_trace.c
+++ src/proc_trace.c
@@ -4,17 +4,22 @@
 #include <stddef.h>
 
 
+static bool handle_ptrace_error(s_proc *proc, bool force)
+{
+    if (errno != ESRCH && !force)
+        return true;
+    proc->ev = PROC_EXITED;
+    proc->exit_status = 1;
+    return false;
+}
+
+
 bool proc_trace(s_proc *proc, enum __ptrace_request request,
                 void *addr, void *data)
 {
     int res = ptrace(request, proc->pid, addr, data);
     if (res < 0)
-    {
-        if (errno != ESRCH)
-            return true;
-        proc->ev = PROC_EXITED;
-        proc->exit_status = 1;
-    }
+        return handle_ptrace_error(proc, false);
     return false;
 }
 
@@ -37,4 +42,29 @@ bool proc_cont(s_proc *proc, enum __ptrace_request request)
 {
     int sig = proc->deliver_signal ? proc->signal : 0;
     return proc_sigtrace(proc, PTRACE_SYSCALL, sig);
+}
+
+
+bool proc_peek(s_proc *proc, void *addr, int *res)
+{
+    errno = 0;
+    *res = ptrace(PTRACE_PEEKTEXT, proc->pid, addr, NULL);
+    if (!errno)
+        return false;
+
+    handle_ptrace_error(proc, true);
+    return true;
+}
+
+
+bool proc_poke(s_proc *proc, void *addr, int data)
+{
+    union
+    {
+        void *vdata;
+        int idata;
+    } orig_data;
+    orig_data.idata = data;
+
+    return proc_trace(proc, PTRACE_POKETEXT, addr, orig_data.vdata);
 }
