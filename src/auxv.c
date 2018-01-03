8c7a59ea5432e819d1fb2a94fe4dab8add25412c
Victor "multun" Collod <victor.collod@epita.fr>
2018-01-03 16:13:47 +0100

    auxv: add an user front-end

--- src/auxv.c
+++ src/auxv.c
@@ -1,5 +1,6 @@
 #include "auxv.h"
 #include "procfs.h"
+#include "commands.h"
 
 #include <err.h>
 #include <fcntl.h>
@@ -23,6 +24,28 @@ bool proc_auxv_get(s_proc *proc, unsigned long key, unsigned long *value)
             *value = cur[1];
             break;
         }
+
+    close(fd);
+
+    if (rd >= 0)
+        return false;
+
+    warn("auxv read for process %d", proc->pid);
+    return true;
+}
+
+
+int CMD(auxv, "prints the auxv",
+        s_proc *proc, int argc, char *argv[])
+{
+    int fd = proc_open_procfs(proc, "auxv", O_RDONLY);
+    if (fd < 0)
+        return true;
+
+    unsigned long cur[2];
+    ssize_t rd;
+    while ((rd = read(fd, cur, sizeof(cur))) > 0)
+        printf("%lx\t%lx\n", cur[0], cur[1]);
 
     close(fd);
 
