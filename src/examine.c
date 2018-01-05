4dc32bc9f15e10e240010cabcb1e2f45ed78999d
Victor "multun" Collod <victor.collod@epita.fr>
2018-01-05 21:22:34 +0100

    examine: implement string and rax format options

--- src/examine.c
+++ src/examine.c
@@ -1,7 +1,44 @@
-#include "commands.h"
-#include "expr.h"
 #include "cmdutils.h"
+#include "commands.h"
 #include "disass.h"
+#include "expr.h"
+#include "proc_trace.h"
+
+#include <string.h>
+
+
+static int print_word(s_proc *proc, const char *format, t_ureg addr)
+{
+    long data;
+    if (proc_peek(proc, (void*)addr, &data))
+        return CMD_FAILURE;
+
+    printf(format, data);
+    return CMD_SUCCESS;
+}
+
+
+static int print_string(s_proc *proc, t_ureg addr)
+{
+    union
+    {
+        long l;
+        char c[sizeof(long) + 1];
+    } data;
+
+    data.c[sizeof(long)] = 0;
+
+    do {
+        if (proc_peek(proc, (void*)addr, &data.l))
+            return CMD_FAILURE;
+        fputs(data.c, stdout);
+        addr += sizeof(long);
+    } while (strlen(data.c) == sizeof(long));
+
+    putchar('\n');
+    return CMD_SUCCESS;
+}
+
 
 int CMD(examine, "reads a {string(s),digit(d),instruction(i)} from [expr]",
         s_proc *proc, int argc, char *argv[])
@@ -16,10 +53,11 @@ int CMD(examine, "reads a {string(s),digit(d),instruction(i)} from [expr]",
     switch (argv[1][0])
     {
     case 'x':
+        return print_word(proc, "%lx\n", addr);
     case 'd':
+        return print_word(proc, "%ld\n", addr);
     case 's':
-        puts("not implemented");
-        return CMD_FAILURE;
+        return print_string(proc, addr);
     case 'i':
         return print_disass(proc, addr, argv[3]) * CMD_FAILURE;
     default:
