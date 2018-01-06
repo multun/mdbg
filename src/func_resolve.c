b1a13c24d7d4556fe3de9ac3a0cc61d931d9c99e
Victor "multun" Collod <victor.collod@epita.fr>
2018-01-06 23:17:52 +0100

    func_resolve: fix function lookup

--- src/func_resolve.c
+++ src/func_resolve.c
@@ -1,7 +1,22 @@
+#include "auxv.h"
 #include "commands.h"
 #include "elfparse.h"
 #include "proc_bin_map.h"
 #include "process.h"
+
+
+bool proc_get_base(s_proc *proc, s_elfimg *vei, Elf64_Addr *res)
+{
+    Elf64_Ehdr *ei = (void*)vei->data;
+    Elf64_Addr bin_ep = ei->e_entry;
+
+    unsigned long mem_ep;
+    if (proc_auxv_get(proc, AT_ENTRY, &mem_ep))
+        return true;
+
+    *res = mem_ep - bin_ep;
+    return false;
+}
 
 
 bool proc_lookup_symbol(s_proc *proc, const char *symname)
@@ -10,13 +25,27 @@ bool proc_lookup_symbol(s_proc *proc, const char *symname)
     if (proc_map_bin(proc, &ei))
         return true;
 
+    Elf64_Addr base;
+    if (proc_get_base(proc, &ei, &base))
+        goto fail;
+
     Elf64_Addr sym = elfimg_symlookup(symname, &ei, 0);
     if (sym == -1)
+    {
         fprintf(stderr, "symbol not found\n");
-    else
-        printf("%lx\n", sym);
+        goto fail;
+    }
 
-    return elfimg_unmap(&ei);
+    printf("%lx\n", sym + base);
+
+    bool failed = false;
+
+exit:
+    return elfimg_unmap(&ei) || failed;
+
+fail:
+    failed = true;
+    goto exit;
 }
 
 
